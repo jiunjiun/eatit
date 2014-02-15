48eece6d50bd4ca404f50fa76abdefd0aec9fbc3
jiunjiun <quietmes@gmail.com>
2014-02-15 12:01:55 +0800

    First eat it

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eatit::Application.config.secret_key_base = '3c45427c07089a2ffacd48c587b76860a1baa40313011c8392d5935fc40619b737c93e09fb86f6cb24178ab3edef4edb4183c74b79bd9c2c46e384569f1fa653'
